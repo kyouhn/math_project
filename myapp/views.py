05068ee7fec04f9f5a48055c00a26bbbb3dc0cc9
kyouhn <kzk0802kazuki@gmail.com>
2018-12-23 22:39:10 +0900

    file 送ったよ

---
+++ myapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+
+def index_template(request):
+    return render(request, 'index.html')
+# Create your views here.
